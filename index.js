1bc56d97b37a0425f4950abf1e8a598591ace066
Barbara Washington <barbara.m.washington@gmail.com>
2018-03-04 21:54:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,10 @@ function sayHiToGrandma(string) {
    return "I can't hear you!"
   }
   
-  
+  function sayHiToGrandma(string) {
+  if (string.toUpperCase() === string) 
+   return "YES INDEED!!"
+  }
   
   
   
