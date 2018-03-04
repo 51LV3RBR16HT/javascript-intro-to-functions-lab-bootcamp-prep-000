d6fe6f38d5bb982d571b0c6a4cfaea6955f3725d
Barbara Washington <barbara.m.washington@gmail.com>
2018-03-04 22:12:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,15 @@ function sayHiToGrandma(string) {
    return "I can't hear you!"
   }
   
-  if ((string)===uppercase) {
+  else if ((string)===uppercase) {
     return 'YES INDEED!'
   }
+  else if ((string)===lowercase) {
+    return "I can't hear you!"
+  }
+  else if ((string)==="I love you, Grandma.")) {
+    return "I love you, too."
+  }
+  }
+}
   
